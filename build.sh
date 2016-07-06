d0c7a3b3e05146ae903f34ac0a77ef1fbd1a2b59
mvdbeek <m.vandenbeek@gmail.com>
2016-07-06 18:45:54 +0200

    debug statement in same shell

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ docker run -d --name rancher rancher/vm-ubuntu
 docker cp rancher:/base_image/ubuntu-14.04-amd64.img ubuntu-14.04-amd64.img
 cp ubuntu-14.04-amd64.img new.qcow2
 qemu-img resize new.qcow2 +5GB
-export LIBGUESTFS_DEBUG=1
-export LIBGUESTFS_TRACE=1
-sudo virt-resize --expand /dev/vda1 ubuntu-14.04-amd64.img new.qcow2
+sudo bash -c "export LIBGUESTFS_DEBUG=1 && \
+              export LIBGUESTFS_TRACE=1 && \
+              virt-resize --expand /dev/vda1 ubuntu-14.04-amd64.img new.qcow2"
 # now we've got a resized partition, so we can build a new docker image
 docker build -t artbio/vm-ubuntu .
